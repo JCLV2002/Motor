6f057d2ac1aafc4a518f3fcca2ba7b8e00e8584c
Carlos <131988778+JCLV2002@users.noreply.github.com>
2024-03-24 16:19:06 -0700

    actualizacion

--- Desktop/Desk.py
+++ Desktop/Desk.py
@@ -1,55 +1,63 @@
 from tkinter import Tk, Label, Button, PhotoImage
 from datetime import datetime
-import os
 
 # Crear la ventana principal
-mi_ventana = Tk()
-mi_ventana.geometry("500x500")
-mi_ventana.title("Mi GUI con Tkinter")
+principal = Tk()
+principal.geometry("400x300")
+principal.title("Control del Motor")
 
-# Crear una etiqueta
-mi_etiqueta = Label(mi_ventana, text="Estado del motor: Detenido", font=("Arial Bold", 20), fg="blue")
-mi_etiqueta.place(x=0, y=100)
+# etiqueta
+estado = Label(principal, text="Estado del motor: \n Detenido", font=("Arial Bold", 20), fg="black")
+estado.place(x=0, y=100)
 
-# Función para manejar el evento del botón
+nota = Label(principal, text="Use las fotos del compañero porque no encontre fotos de mismo tamaño", font=("Arial Bold", 8), fg="black")
+nota.place(x=0, y=250)
+
+# Funciónes de los botones
 def girar_izquierda():
-    mi_etiqueta.config(text="Estado del motor: Girando en sentido antihorario")
-    mi_boton2.config(state="normal")
-    mi_boton3.config(state="normal")
-    mi_boton.config(state="disabled")
+    estado.config(text="Estado del motor: \n Girando a la izquierda")
+    detener.config(state="normal")
+    derecha.config(state="normal")
+    izquierda.config(state="disabled")
 
 def girar_derecha():
-    mi_etiqueta.config(text="Estado del motor: Girando en sentido horario")
-    mi_boton2.config(state="normal")
-    mi_boton3.config(state="disabled")
-    mi_boton.config(state="normal")
+    estado.config(text="Estado del motor: \n Girando a la derecha")
+    detener.config(state="normal")
+    derecha.config(state="disabled")
+    izquierda.config(state="normal")
 
 def detener_motor():
-    mi_etiqueta.config(text="Estado del motor: Detenido")
-    mi_boton2.config(state="disabled")
-    mi_boton3.config(state="normal")
-    mi_boton.config(state="normal")
+    estado.config(text="Estado del motor:\n Detenido")
+    detener.config(state="disabled")
+    derecha.config(state="normal")
+    izquierda.config(state="normal")
 
 def actualizar_fecha_hora():
     fecha_hora_actual = datetime.now().strftime("%H:%M:%S - %Y-%m-%d")
-    mi_etiqueta2.config(text=fecha_hora_actual)
-    mi_ventana.after(1000, actualizar_fecha_hora)  # Actualizar cada segundo
+    etiqueta_fecha_hora.config(text=fecha_hora_actual)
+    principal.after(1000, actualizar_fecha_hora)  # Actualizar cada segundo
 
-# Crear una etiqueta para la fecha y hora
-mi_etiqueta2 = Label(mi_ventana, text="", font=("Arial Bold", 20), fg="blue")
-mi_etiqueta2.place(x=0, y=200)
+# Colocar la etiqueta de la fecha y hora
+etiqueta_fecha_hora = Label(principal, text="", font=("Arial Bold", 20), fg="black")
+etiqueta_fecha_hora.place(x=0, y=200)
 
-# Crear un botón
-imagen = PhotoImage(file="flecha-izquierda.png")
-mi_boton = Button(mi_ventana, image=imagen, command=girar_izquierda)
-mi_boton.place(x=0, y=0)
-imagen2 = PhotoImage(file="pausa.png")
-mi_boton2 = Button(mi_ventana, image=imagen2, command=detener_motor, state="disabled")
-mi_boton2.place(x=100, y=0)
-imagen3 = PhotoImage(file="flecha-correcta.png")
-mi_boton3 = Button(mi_ventana, image=imagen3, command=girar_derecha)
-mi_boton3.place(x=200, y=0)
+# poner imagenes en los botones
+imagen_izquierda = PhotoImage(file="C:/Users/jesus/OneDrive/Documentos/GitHub/Motor/Desktop/fotos/izquierda.png")
+izquierda = Button(principal, image=imagen_izquierda, command=girar_izquierda)
+izquierda.place(x=0, y=0)
 
+imagen_detener = PhotoImage(file="C:/Users/jesus/OneDrive/Documentos/GitHub/Motor/Desktop/fotos/pausa.png")
+detener = Button(principal, image=imagen_detener, command=detener_motor, state="disabled")
+detener.place(x=100, y=0)
+
+imagen_derecha = PhotoImage(file="C:/Users/jesus/OneDrive/Documentos/GitHub/Motor/Desktop/fotos/derecha.png")
+derecha = Button(principal, image=imagen_derecha, command=girar_derecha)
+derecha.place(x=200, y=0)
+
+salir = Button(principal, text="Salir", command=principal.quit, font=("Arial Bold", 15), fg="black")
+salir.place(x=300, y=150)
+# fecha y hora
 actualizar_fecha_hora()
-# Ejecutar el bucle de eventos
-mi_ventana.mainloop()
+
+# while infinito
+principal.mainloop()
